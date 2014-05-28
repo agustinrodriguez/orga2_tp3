995046e49c57004d1151096a38ff32f197a9bad2
Agustin <agustin@agustin-K53SC.(none)>
2014-05-27 21:53:53 -0300

    Primer ejercicio

---
+++ src/gdt.c
@@ -0,0 +1,128 @@
+/* ** por compatibilidad se omiten tildes **
+================================================================================
+ TRABAJO PRACTICO 3 - System Programming - ORGANIZACION DE COMPUTADOR II - FCEN
+================================================================================
+  definicion de la tabla de descriptores globales
+*/
+
+#include "gdt.h"
+
+gdt_entry gdt[GDT_COUNT] = {
+    /* Descriptor nulo*/
+    /* Offset = 0x00 */
+    [GDT_IDX_NULL_DESC] = (gdt_entry) {
+        (unsigned short)    0x0000,         /* limit[0:15]  */
+        (unsigned short)    0x0000,         /* base[0:15]   */
+        (unsigned char)     0x00,           /* base[23:16]  */
+        (unsigned char)     0x00,           /* type         */
+        (unsigned char)     0x00,           /* s            */
+        (unsigned char)     0x00,           /* dpl          */
+        (unsigned char)     0x00,           /* p            */
+        (unsigned char)     0x00,           /* limit[16:19] */
+        (unsigned char)     0x00,           /* avl          */
+        (unsigned char)     0x00,           /* l            */
+        (unsigned char)     0x00,           /* db           */
+        (unsigned char)     0x00,           /* g            */
+        (unsigned char)     0x00,           /* base[31:24]  */
+    },
+    
+    /* segmento de codigo */
+    /* Offset = 0x09 */
+    /* limit decimal 187648 */
+    /* limit hexa 0x2DD00 */
+    [GDT_IDX_CODE_0] = (gdt_entry) {
+        (unsigned short)    0xDD00,         /* limit[0:15]  */
+        (unsigned short)    0x0000,         /* base[0:15]   */
+        (unsigned char)     0x00,           /* base[23:16]  */
+        (unsigned char)     0x0A,           /* type         */
+        (unsigned char)     0x01,           /* s            */
+        (unsigned char)     0x00,           /* dpl          */
+        (unsigned char)     0x01,           /* p            */
+        (unsigned char)     0x02,           /* limit[16:19] */
+        (unsigned char)     0x00,           /* avl          */
+        (unsigned char)     0x00,           /* l            */
+        (unsigned char)     0x01,           /* db           */
+        (unsigned char)     0x01,           /* g            */
+        (unsigned char)     0x00,           /* base[31:24]  */
+    },
+
+    /* segmento de codigo */
+    /* Offset = 0x0A */
+    [GDT_IDX_CODE_3] = (gdt_entry) {
+        (unsigned short)    0xDD00,         /* limit[0:15]  */
+        (unsigned short)    0x0000,         /* base[0:15]   */
+        (unsigned char)     0x00,           /* base[23:16]  */
+        (unsigned char)     0x0A,           /* type         */
+        (unsigned char)     0x01,           /* s            */
+        (unsigned char)     0x03,           /* dpl          */
+        (unsigned char)     0x01,           /* p            */
+        (unsigned char)     0x02,           /* limit[16:19] */
+        (unsigned char)     0x00,           /* avl          */
+        (unsigned char)     0x00,           /* l            */
+        (unsigned char)     0x01,           /* db           */
+        (unsigned char)     0x01,           /* g            */
+        (unsigned char)     0x00,           /* base[31:24]  */
+    },
+
+    /* segmento de dato */
+    /* Offset = 0x0B */
+    [GDT_IDX_DATA_0] = (gdt_entry) {
+        (unsigned short)    0xDD00,         /* limit[0:15]  */
+        (unsigned short)    0x0000,         /* base[0:15]   */
+        (unsigned char)     0x00,           /* base[23:16]  */
+        (unsigned char)     0x02,           /* type         */
+        (unsigned char)     0x01,           /* s            */
+        (unsigned char)     0x00,           /* dpl          */
+        (unsigned char)     0x01,           /* p            */
+        (unsigned char)     0x02,           /* limit[16:19] */
+        (unsigned char)     0x00,           /* avl          */
+        (unsigned char)     0x00,           /* l            */
+        (unsigned char)     0x01,           /* db           */
+        (unsigned char)     0x01,           /* g            */
+        (unsigned char)     0x00,           /* base[31:24]  */
+    },
+
+    /* segmento de dato */
+    /* Offset = 0x0C */
+    [GDT_IDX_DATA_3] = (gdt_entry) {
+        (unsigned short)    0xDD00,         /* limit[0:15]  */
+        (unsigned short)    0x0000,         /* base[0:15]   */
+        (unsigned char)     0x00,           /* base[23:16]  */
+        (unsigned char)     0x02,           /* type         */
+        (unsigned char)     0x01,           /* s            */
+        (unsigned char)     0x03,           /* dpl          */
+        (unsigned char)     0x01,           /* p            */
+        (unsigned char)     0x02,           /* limit[16:19] */
+        (unsigned char)     0x00,           /* avl          */
+        (unsigned char)     0x00,           /* l            */
+        (unsigned char)     0x01,           /* db           */
+        (unsigned char)     0x01,           /* g            */
+        (unsigned char)     0x00,           /* base[31:24]  */
+    },
+
+    /* segmento de video */
+    /* Offset = 0x0D */
+    /* base 0xB8000 */
+    /* limit 4000 (0xFA0) */
+
+    [GDT_IDX_VIDEO] = (gdt_entry) {
+        (unsigned short)    0x0FA0,         /* limit[0:15]  */
+        (unsigned short)    0x8000,         /* base[0:15]   */
+        (unsigned char)     0x0B,           /* base[23:16]  */
+        (unsigned char)     0x02,           /* type         */
+        (unsigned char)     0x01,           /* s            */
+        (unsigned char)     0x00,           /* dpl          */
+        (unsigned char)     0x01,           /* p            */
+        (unsigned char)     0x00,           /* limit[16:19] */
+        (unsigned char)     0x00,           /* avl          */
+        (unsigned char)     0x00,           /* l            */
+        (unsigned char)     0x01,           /* db           */
+        (unsigned char)     0x01,           /* g            */
+        (unsigned char)     0x00,           /* base[31:24]  */
+    },
+};
+
+gdt_descriptor GDT_DESC = {
+    sizeof(gdt) - 1,
+    (unsigned int) &gdt
+};
---
+++ src/kernel.asm
@@ -0,0 +1,124 @@
+; ** por compatibilidad se omiten tildes **
+; ==============================================================================
+; TRABAJO PRACTICO 3 - System Programming - ORGANIZACION DE COMPUTADOR II - FCEN
+; ==============================================================================
+
+%include "imprimir.mac"
+
+global start
+
+
+;; Saltear seccion de datos
+jmp start
+
+;;
+;; Seccion de datos.
+;; -------------------------------------------------------------------------- ;;
+iniciando_mr_msg db     'Iniciando kernel (Modo Real)...'
+iniciando_mr_len equ    $ - iniciando_mr_msg
+
+iniciando_mp_msg db     'Iniciando kernel (Modo Protegido)...'
+iniciando_mp_len equ    $ - iniciando_mp_msg
+
+;;
+;; Seccion de código.
+;; -------------------------------------------------------------------------- ;;
+
+;; Punto de entrada del kernel.
+BITS 16
+start:
+    ; Deshabilitar interrupciones
+    cli
+
+    ; Cambiar modo de video a 80 X 50
+    mov ax, 0003h
+    int 10h ; set mode 03h
+    xor bx, bx
+    mov ax, 1112h
+    int 10h ; load 8x8 font
+
+    ; Imprimir mensaje de bienvenida
+    imprimir_texto_mr iniciando_mr_msg, iniciando_mr_len, 0x07, 0, 0
+    
+
+    ; Habilitar A20
+    call habilitar_a20
+    ;cli
+
+    ; Cargar la GDT
+    LGDT [GDT_DESC]
+    
+    ; Setear el bit PE del registro CR0
+    mov EAX, CR0
+    or EAX, 1
+    mov CR0, EAX
+
+    ; Saltar a modo protegido
+    
+    jmp 0x48:modo_protegido ; 0100 1 | 0 | 00
+
+    BITS 32
+    modo_protegido:
+
+    ; Establecer selectores de segmentos
+    xor EAX, EAX 
+    mov AX, 0X48    ;0100 1000 ; cargo el selector de segmento de datos level 0: 9|0|00
+    mov DS, AX
+    mov ES, AX
+    mov GS, AX
+    mov SS, AX
+
+    mov AX, 0x68 ; 0110 1000 ;cargo el selector de segmento de video 13 | 0 | 00
+    mov FS, AX
+
+    ; Establecer la base de la pila
+    mov EBP, 0x27000
+    mov ESP, EBP
+
+    ; Imprimir mensaje de bienvenida
+    
+    ; Inicializar pantalla
+    
+    ; Inicializar el manejador de memoria
+    
+    ; Inicializar el directorio de paginas
+    
+    ; Cargar directorio de paginas
+    
+    ; Habilitar paginacion
+    
+    ; Inicializar tss
+    
+    ; Inicializar tss de la tarea Idle
+    
+    ; Inicializar tss de las tanques
+    
+    ; Inicializar el scheduler
+    
+    ; Inicializar la IDT
+    
+    ; Inicializar Game
+    
+    ; Cargar IDT
+    
+    ; Configurar controlador de interrupciones
+    
+    ; pintar posiciones inciales de tanques
+    
+    ; Cargar tarea inicial
+
+    ; Habilitar interrupciones
+ 
+    ; Saltar a la primera tarea: Idle
+
+    ; Ciclar infinitamente (por si algo sale mal...)
+    mov eax, 0xFFFF
+    mov ebx, 0xFFFF
+    mov ecx, 0xFFFF
+    mov edx, 0xFFFF
+    jmp $
+    jmp $
+
+;; -------------------------------------------------------------------------- ;;
+
+%include "a20.asm"
